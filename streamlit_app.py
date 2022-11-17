fe8853902c836ef070a0cea20264fb1f0a2d284c
Blatant Builder <116133889+blatant-builder@users.noreply.github.com>
2022-11-16 22:55:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,12 @@ df = pandas.DataFrame(my_catalog)
 
 # put the first column into a list
 color_list = df[0].values.tolist()
-print(color_list)
+# print(color_list)
 
 # Let's put a pick list here so they can pick the color
-#option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+
 
 
 
